e1ee133aea2747878abf2f46e4797fccb34a550e
youmu257 <youmu257@gmail.com>
2021-10-13 23:04:50 +0800

    feat: 語音播放按鈕(測試版)

--- src/components/VoicePage.vue
+++ src/components/VoicePage.vue
@@ -6,6 +6,21 @@
         <a href="https://www.youtube.com/c/STORIANarrator%E9%81%8A%E6%88%B2%E5%B7%A5%E4%BD%9C%E5%AE%A4/about" target="_blank" rel="noopener">Youtube</a><br>
         <a href="https://twitter.com/Chilla_Storia" target="_blank" rel="noopener">Twitter</a>
     </p>
+    <h3>Button</h3>
+    <audio ref="subaru-1"
+        class="sound-play"
+        :src="require('@/assets/sound/subaru-1.mp3')"
+        controls
+        preload
+        ></audio>
+    <div class="toggle-subaru-1 paused" @click="play('subaru-1')">播放/暫停1</div>
+    <audio ref="subaru-3"
+        class="sound-play"
+        :src="require('@/assets/sound/subaru-3.mp3')"
+        controls
+        preload
+        ></audio>
+    <div class="toggle-subaru-3 paused" @click="play('subaru-3')">播放/暫停3</div>
     </div>
 </template>
 
@@ -14,6 +29,21 @@ export default {
     name: 'VideoPage',
     props: {
         msg: String
+    },
+    methods: {
+        play(id) {
+            let audio = this.$refs[id];
+            if (audio.paused && document.querySelector(".toggle-" + id).classList.contains("paused")) {
+                console.log("play it");
+                audio.play();
+                document.querySelector(".toggle-" + id).classList.remove("paused");
+            } else {
+                // TODO: 暫停操作上感覺怪怪的 之後調整
+                console.log("pause it");
+                audio.pause();
+                document.querySelector(".toggle-" + id).classList.add("paused");
+            }
+        }
     }
 }
 </script>
@@ -33,5 +63,8 @@ li {
 }
 a {
     color: #42b983;
+}
+.sound-play {
+    display: none;
 }
 </style>
