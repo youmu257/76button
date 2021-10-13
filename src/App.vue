303eaf85acced8377e0c62129f3c153167400415
youmu257 <youmu257@gmail.com>
2021-10-13 21:38:01 +0800

    style: 調成4個空白縮排，重新命名主頁面

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <VoicePage msg="祈菈‧貝希毛絲的語音按鈕"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VoicePage from './components/VoicePage.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        VoicePage
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/VoicePage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+        祈菈的資訊<br>
+        <a href="https://www.youtube.com/c/STORIANarrator%E9%81%8A%E6%88%B2%E5%B7%A5%E4%BD%9C%E5%AE%A4/about" target="_blank" rel="noopener">Youtube</a><br>
+        <a href="https://twitter.com/Chilla_Storia" target="_blank" rel="noopener">Twitter</a>
+    </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VideoPage',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+</style>
