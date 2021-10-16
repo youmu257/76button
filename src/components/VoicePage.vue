a45eca8cca47b31501d861e63109ec3017ebc107
youmu257 <youmu257@gmail.com>
2021-10-17 01:50:31 +0800

    feat: 把 css 獨立出來

--- src/components/VoicePage.vue
+++ src/components/VoicePage.vue
@@ -53,28 +53,4 @@ export default {
     },
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-    margin: 40px 0 0;
-}
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-a {
-    color: #42b983;
-}
-.background {
-    background-color:#70e4d0;
-    padding:10px;
-    margin-bottom:5px;
-    border:2px #ccc solid;
-    border-radius:10px;
-}
-</style>
+<style scoped src="../css/VoiceButton.css"></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+require('@/css/App.css');
+
 createApp(App).mount('#app')
