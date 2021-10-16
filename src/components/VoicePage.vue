ed5bb94bed97b5dc4760046da0522aba6411c3de
youmu257 <youmu257@gmail.com>
2021-10-17 01:03:46 +0800

    feat: 依照資料產生按鈕和類別區塊

--- src/components/VoicePage.vue
+++ src/components/VoicePage.vue
@@ -1,18 +1,23 @@
 <template>
     <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-        祈菈的資訊<br>
-        <a href="https://www.youtube.com/c/STORIANarrator%E9%81%8A%E6%88%B2%E5%B7%A5%E4%BD%9C%E5%AE%A4/about" target="_blank" rel="noopener">Youtube</a><br>
-        <a href="https://twitter.com/Chilla_Storia" target="_blank" rel="noopener">Twitter</a>
-    </p>
-    <h3>Button</h3>
-    <VoiceButton
-        voiceFileName="76-1"
-        buttonName="語音1" />
-    <VoiceButton
-        voiceFileName="76-2"
-        buttonName="語音2" />
+        <h1>{{ msg }}</h1>
+        <p>
+            祈菈的資訊<br>
+            <a href="https://www.youtube.com/c/STORIANarrator%E9%81%8A%E6%88%B2%E5%B7%A5%E4%BD%9C%E5%AE%A4/about" target="_blank" rel="noopener">Youtube</a><br>
+            <a href="https://twitter.com/Chilla_Storia" target="_blank" rel="noopener">Twitter</a>
+        </p>
+        <div
+            class="background"
+            v-for="(item, index) in btnMap"
+            :key="index">
+            <h3>{{ item.category }}</h3>
+            <VoiceButton
+                v-for="(btnData, index) in item.btnList"
+                :key="index"
+                :voiceFileName=btnData.fileName
+                :buttonName=btnData.btnName>
+            </VoiceButton>
+        </div>
     </div>
 </template>
 
@@ -26,7 +31,26 @@ export default {
     },
     props: {
         msg: String
-    }
+    },
+    data() {
+        return {
+            btnMap: [
+                {
+                    category: "祈哥",
+                    btnList: [
+                        {
+                            fileName: "76-1",
+                            btnName: "起司我了"
+                        },
+                        {
+                            fileName: "76-2",
+                            btnName: "起司我了2"
+                        }
+                    ]
+                },
+            ]
+        }
+    },
 }
 </script>
 
@@ -45,5 +69,12 @@ li {
 }
 a {
     color: #42b983;
+}
+.background {
+    background-color:#70e4d0;
+    padding:10px;
+    margin-bottom:5px;
+    border:2px #ccc solid;
+    border-radius:10px;
 }
 </style>
