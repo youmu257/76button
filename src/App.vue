1a838a0266eecfa758afffefc80f193a49292cb7
youmu257 <youmu257@gmail.com>
2021-10-17 23:26:57 +0800

    feat: 新增 footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <img alt="Vue logo" src="./assets/logo.png">
     <VoicePage msg="祈菈‧貝希毛絲的語音按鈕"/>
+    <VoicePageFooter/>
 </template>
 
 <script>
 import VoicePage from './components/VoicePage.vue'
+import VoicePageFooter from './components/VoicePageFooter.vue'
 
 export default {
     name: 'App',
     components: {
         VoicePage,
+        VoicePageFooter,
     }
 }
 </script>
