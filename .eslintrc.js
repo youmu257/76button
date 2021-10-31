d82213c336cb08534f732953a4c641a54133dd23
youmu257 <youmu257@gmail.com>
2021-10-31 15:21:55 +0800

    feat: 新增 eslint 與 style 設定檔

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': ['plugin:vue/essential', 'eslint:recommended', 'plugin:vue/recommended'],
+  'parserOptions': {
+    'parser': 'babel-eslint'
+  },
+  'rules': {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+  }
+}
