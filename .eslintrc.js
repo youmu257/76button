b08b4b8256a87b9e516f5d37fe17c3dd68ced38a
youmu257 <youmu257@gmail.com>
2021-10-31 20:12:57 +0800

    feat: 換行強制為 unix 移除

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
   },
   'rules': {
     'indent': ['error', 2],
-    'linebreak-style': ['error', 'unix'],
     'quotes': ['error', 'single'],
     'semi': ['error', 'never'],
   }
