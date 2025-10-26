5db04d24fd69ca69e4d57745d2f99df444d359b0
LEO <youmu257@gmail.com>
2025-10-26 23:48:07 +0800

    refactor: 抽離社群媒體連結為獨立組件並統一圖示樣式
    - 新增 InformationSidebar.vue 組件
    - 統一所有社群媒體圖示尺寸為 32px
    - 優化圖示間隔一致性
    - 新增 Twitch 連結及品牌色樣式
    - 新增 買動漫, 三視圖, 官方網站連結

--- src/components/CollapseSidebar.vue
+++ src/components/CollapseSidebar.vue
@@ -78,127 +78,14 @@
       <hr class="sidebar-divider">
 
       <!-- 社群媒體連結容器 -->
-      <div class="social-media-container">
-        <!-- YouTube 連結 -->
-        <a
-          href="https://www.youtube.com/c/STORIANarrator%E9%81%8A%E6%88%B2%E5%B7%A5%E4%BD%9C%E5%AE%A4/about"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="前往 YouTube 頻道"
-          class="social-link"
-        >
-          <i
-            class="bi bi-youtube"
-            aria-hidden="true"
-          />
-          <span
-            v-if="isSidebarOpen"
-            class="social-name"
-          >YouTube</span>
-        </a>
-
-        <!-- Twitter 連結 -->
-        <a
-          href="https://twitter.com/Chilla_Storia"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="前往 Twitter"
-          class="social-link"
-        >
-          <i
-            class="bi bi-twitter"
-            aria-hidden="true"
-          />
-          <span
-            v-if="isSidebarOpen"
-            class="social-name"
-          >Twitter</span>
-        </a>
-
-        <!-- Discord 連結 -->
-        <a
-          href="https://discord.gg/Y4fp3k2"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="加入 Discord 伺服器"
-          class="social-link"
-        >
-          <i
-            class="bi bi-discord"
-            aria-hidden="true"
-          />
-          <span
-            v-if="isSidebarOpen"
-            class="social-name"
-          >Discord</span>
-        </a>
-
-        <!-- Facebook 連結 -->
-        <a
-          href="https://www.facebook.com/Chilla76?locale=zh_TW"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="前往 Facebook 粉絲專頁"
-          class="social-link"
-        >
-          <i
-            class="bi bi-facebook"
-            aria-hidden="true"
-          />
-          <span
-            v-if="isSidebarOpen"
-            class="social-name"
-          >Facebook</span>
-        </a>
-
-        <!-- Plurk 連結 -->
-        <a
-          href="https://www.plurk.com/Chilla76"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="前往 Plurk"
-          class="social-link"
-        >
-          <img
-            width="32"
-            height="32"
-            :src="require('@/assets/plurk.png')"
-            alt="Plurk"
-            loading="lazy"
-          >
-          <span
-            v-if="isSidebarOpen"
-            class="social-name"
-          >Plurk</span>
-        </a>
-
-        <!-- Bluesky 連結 -->
-        <a
-          href="https://bsky.app/profile/chillastoria.bsky.social"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="前往 Bluesky"
-          class="social-link"
-        >
-          <img
-            width="32"
-            height="28"
-            src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Bluesky_Logo.svg"
-            alt="Bluesky"
-            loading="lazy"
-          >
-          <span
-            v-if="isSidebarOpen"
-            class="social-name"
-          >Bluesky</span>
-        </a>
-      </div>
+      <InformationSidebar :is-sidebar-open="isSidebarOpen" />
     </nav>
   </div>
 </template>
 
 <script>
 import sidebarItemList from '../assets/sidebar-list.json'
+import InformationSidebar from './InformationSidebar.vue'
 
 /**
  * CollapseSidebar 組件
@@ -218,6 +105,10 @@ import sidebarItemList from '../assets/sidebar-list.json'
  */
 export default {
   name: 'CollapseSidebar',
+
+  components: {
+    InformationSidebar,
+  },
 
   data() {
     return {
@@ -287,4 +178,3 @@ export default {
 <!-- 引入外部樣式表 -->
 <style scoped src="../css/CollapseSidebar.css"></style>
 <style scoped src="../css/ItemIcon.css"></style>
-
---
+++ src/components/InformationSidebar.vue
@@ -0,0 +1,229 @@
+<template>
+  <!-- 社群媒體連結容器 -->
+  <div class="social-media-container">
+    <!-- YouTube 連結 -->
+    <a
+      href="https://www.youtube.com/c/STORIANarrator%E9%81%8A%E6%88%B2%E5%B7%A5%E4%BD%9C%E5%AE%A4/about"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="前往 YouTube 頻道"
+      class="social-link"
+    >
+      <i
+        class="bi bi-youtube"
+        aria-hidden="true"
+      />
+      <span
+        v-if="isSidebarOpen"
+        class="social-name"
+      >YouTube</span>
+    </a>
+
+    <!-- Twitch 連結 -->
+    <a
+      href="https://www.twitch.tv/storiavtuber00"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="前往 Twitch 頻道"
+      class="social-link"
+    >
+      <i
+        class="bi bi-twitch"
+        aria-hidden="true"
+      />
+      <span
+        v-if="isSidebarOpen"
+        class="social-name"
+      >Twitch</span>
+    </a>
+
+    <!-- Twitter 連結 -->
+    <a
+      href="https://twitter.com/Chilla_Storia"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="前往 Twitter"
+      class="social-link"
+    >
+      <i
+        class="bi bi-twitter"
+        aria-hidden="true"
+      />
+      <span
+        v-if="isSidebarOpen"
+        class="social-name"
+      >X (Twitter)</span>
+    </a>
+
+    <!-- Discord 連結 -->
+    <a
+      href="https://discord.gg/Y4fp3k2"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="加入 Discord 伺服器"
+      class="social-link"
+    >
+      <i
+        class="bi bi-discord"
+        aria-hidden="true"
+      />
+      <span
+        v-if="isSidebarOpen"
+        class="social-name"
+      >Discord</span>
+    </a>
+
+    <!-- Facebook 連結 -->
+    <a
+      href="https://www.facebook.com/Chilla76?locale=zh_TW"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="前往 Facebook 粉絲專頁"
+      class="social-link"
+    >
+      <i
+        class="bi bi-facebook"
+        aria-hidden="true"
+      />
+      <span
+        v-if="isSidebarOpen"
+        class="social-name"
+      >Facebook</span>
+    </a>
+
+    <!-- Plurk 連結 -->
+    <a
+      href="https://www.plurk.com/Chilla76"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="前往 Plurk"
+      class="social-link"
+    >
+      <img
+        width="32"
+        height="32"
+        :src="require('@/assets/plurk.png')"
+        alt="Plurk"
+        loading="lazy"
+      >
+      <span
+        v-if="isSidebarOpen"
+        class="social-name"
+      >Plurk</span>
+    </a>
+
+    <!-- Bluesky 連結 -->
+    <a
+      href="https://bsky.app/profile/chillastoria.bsky.social"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="前往 Bluesky"
+      class="social-link"
+    >
+      <img
+        width="32"
+        height="32"
+        src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Bluesky_Logo.svg"
+        alt="Bluesky"
+        loading="lazy"
+      >
+      <span
+        v-if="isSidebarOpen"
+        class="social-name"
+      >Bluesky</span>
+    </a>
+
+    <!-- 買動漫連結 -->
+    <a
+      href="https://www.myacg.com.tw/seller_market.php?seller=244163"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="前往買動漫"
+      class="social-link"
+    >
+      <img
+        width="32"
+        height="32"
+        src="https://cdn.myacg.com.tw/images/favicon.ico"
+        alt="MyACG"
+        loading="lazy"
+      >
+      <span
+        v-if="isSidebarOpen"
+        class="social-name"
+      >買動漫</span>
+    </a>
+
+    <!-- 三視圖連結 -->
+    <a
+      href="https://bsky.app/profile/chillastoria.bsky.social/post/3l6p664qkim25"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Views of Objects"
+      class="social-link"
+    >
+      <img
+        width="32"
+        height="32"
+        src="https://chilla76cheese.com/favicon.ico"
+        alt="Views of Objects"
+        loading="lazy"
+      >
+      <span
+        v-if="isSidebarOpen"
+        class="social-name"
+      >三視圖</span>
+    </a>
+
+    <!-- 官方網站連結 -->
+    <a
+      href="https://lit.link/zh-tw/chilla"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="lit.link"
+      class="social-link"
+    >
+      <img
+        width="32"
+        height="32"
+        src="https://lit.link/favicon.ico"
+        alt="lit.link"
+        loading="lazy"
+      >
+      <span
+        v-if="isSidebarOpen"
+        class="social-name"
+      >官方網站</span>
+    </a>
+  </div>
+</template>
+
+<script>
+/**
+ * InformationSidebar 組件
+ *
+ * 用途：
+ * - 顯示社群媒體連結
+ * - 支援側邊欄收合狀態，動態顯示/隱藏社群名稱
+ *
+ * @component
+ * @prop {Boolean} isSidebarOpen - 側邊欄是否展開
+ */
+export default {
+  name: 'InformationSidebar',
+
+  props: {
+    /**
+     * 側邊欄開啟狀態
+     * @type {Boolean}
+     * @required
+     */
+    isSidebarOpen: {
+      type: Boolean,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped src="../css/CollapseSidebar.css"></style>
